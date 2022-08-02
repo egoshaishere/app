863350f60d1e14064d5376f3e2534dd3abf6a648
Егор <eremin.e@m-style.spb.ru>
2022-08-02 10:21:47 +0300

    add draggable feature

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,33 +1,57 @@
 <template>
-  <div class="card">
-    <div class="header__card">
-      <div>{{ card.id }}</div>
-      <div>{{ card.title }}</div>
-      <div class="delete__card" v-on:click="del">&#128465;</div>
-      <div class="edit__card" v-on:click="edit">&#x1F589;</div>
-    </div>
-    <div class="header__body">
-      <div>{{ card.body }}</div>
+  <div v-draggable>
+    <div class="card">
+      <div class="header__card">
+        <div>{{ card.id }}</div>
+        <div>{{ card.title }}</div>
+        <div class="delete__card" v-on:click="del">&#128465;</div>
+        <div class="edit__card" v-on:click="edit">&#x1F589;</div>
+      </div>
+      <div class="header__body">
+        <div>{{ card.body }}</div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import { mapActions } from "vuex";
+import { Draggable } from "draggable-vue-directive";
 
 export default {
   name: "Card",
+  directives: {
+    Draggable,
+  },
   props: {
     card: {},
+  },
+  data() {
+    return {
+      options: {
+        position: {
+          right: 120,
+          top: 120,
+        },
+        edgeSpace: [100, 100, 100, 100],
+      },
+    };
   },
   methods: {
     ...mapActions(["deleteCard"]),
     del() {
       this.deleteCard(this.card.id);
     },
-    edit() {
-
-    }
+    edit() {},
+    start(point) {
+      console.log(point);
+    },
+    move(point) {
+      console.log(point);
+    },
+    end(point) {
+      console.log(point);
+    },
   },
 };
 </script>
@@ -37,19 +61,15 @@ export default {
   border: solid 2px black;
   border-radius: 10px;
   width: 200px;
-
   margin: 10px;
-  /* padding: 10px; */
 }
 
 .header__card {
   display: flex;
   flex-direction: row;
   justify-content: space-around;
-
   background-color: black;
   color: aliceblue;
-
   border-radius: 10px 10px 0px 0px;
 }
 
